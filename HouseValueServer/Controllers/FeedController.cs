6435999375f5fdae41b8535eb4a492bb519c0645
rapanuga <rapanuga@microsoft.com>
2017-02-28 16:47:46 -0800

    Enable Cors; Dummy Estimate

--- HouseValueServer/Controllers/FeedController.cs
+++ HouseValueServer/Controllers/FeedController.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace CelebriTweesServer.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class FeedController : ApiController
     {
-        
+
         public FeedController()
         {
         }
@@ -15,15 +18,75 @@ namespace CelebriTweesServer.Controllers
         [Route("feed/postarticle")]
         public IEnumerable<string> myAction()
         {
-            return new[] { "value2" };
+            return new[] {"value2"};
         }
 
         [HttpGet]
         [Route("feed/getfeed/{request}")]
         public IEnumerable<string> GetNewsFeed(string request)
         {
-            return new[] { "value1", "value2" };
+            return new[] {"value1", "value2"};
         }
 
+        [HttpPost]
+        [Route("feed/getestimate")]
+        public double GetEstimate([FromBody] Property property)
+        {
+            return 5000;
+        }
+
+    }
+
+    [DataContract]
+    public class Test
+    {
+        [DataMember]
+        public string Id { get; set; }
+    }
+
+
+    [DataContract]
+    public class Property
+    {
+        [DataMember]
+        public LatLng LatLng { get; set; }
+        [DataMember]
+        public int YearConstructed { get; set; }
+        [DataMember]
+        public double BuiltUpArea { get; set; }
+        [DataMember]
+        public double PlotSize { get; set; }
+        [DataMember]
+        public int Bathrooms { get; set; }
+        [DataMember]
+        public int Bedrooms { get; set; }
+    }
+
+    [DataContract]
+    public enum PropertyType
+    {
+        [EnumMember]
+        MultistoreyApartment,
+        [EnumMember]
+        BuilderFloorApartment,
+        [EnumMember]
+        ResidentialHouse,
+        [EnumMember]
+        Villa,
+        [EnumMember]
+        ResidentialPlot,
+        [EnumMember]
+        Penthouse,
+        [EnumMember]
+        Studio
+    }
+
+    [DataContract]
+    public class LatLng
+    {
+        [DataMember]
+        public double Lat { get; set; }
+        [DataMember]
+        public double Lng { get; set; }
     }
 }
