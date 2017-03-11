c044147f231fdf2f8211b753b3184d8e6bf29365
panuganti <rajkiran.panuganti@gmail.com>
2017-03-10 18:12:32 -0800

    Fix Publishing

--- HouseValueServer/Controllers/FeedController.cs
+++ HouseValueServer/Controllers/FeedController.cs
@@ -44,7 +44,7 @@ namespace HouseValueServer.Controllers
         [Route("feed/getestimate")]
         public async Task<double> GetEstimate([FromBody] Property house)
         {
-            apiKey = "";
+            apiKey = "GylIN/LmsVKP+yKLYjebrvPFfir2W9h08X2Rfz9l+oVq/nL4swwDPMqz4fZbv13i5YMV9ArzA4+rI5CddGlMQA==";
             var housingData = new HousingData();
             housingData.age_of_property = Convert.ToInt32((DateTime.Now - new DateTime(house.YearConstructed, 1, 1)).TotalDays / 30);
             housingData.bathroom_count = house.Bathrooms;
@@ -60,7 +60,7 @@ namespace HouseValueServer.Controllers
             housingData.under_construction = house.UnderConstruction;
             var featureVector = Utils.HousingFeatures(housingData).ToArray();
             var value = await ServiceCall.InvokeRequestResponseService(featureVector, apiKey);
-            return Convert.ToInt32(value * housingData.built_up_area);
+            return Convert.ToInt32(1000*(Convert.ToInt32(value * housingData.built_up_area/1000)));
         }
     }
 
